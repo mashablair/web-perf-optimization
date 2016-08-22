42562a1da964e31423a39089ac5fd7524da2e646
Maria Blair <maria.klachko@gmail.com>
2016-08-22 15:59:37 -0700

    Minified perfmatters.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,30 +8,37 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    responsive_images: {
-      dev: {
-        files: [{
-          expand: true,
-          cwd: 'views/images/',            //Removed 'images_src/'
-          src: ['**/*.{gif,jpg,png}'],
-          dest: 'dist/images/'
-        }]
-      }
-    }
-    // imagemin: {
+    // responsive_images: {
     //   dev: {
     //     files: [{
-    //       expand: true,                  // Enable dynamic expansion
-    //       cwd: 'views/images/',           //Removed 'images_src' to minify images in Cam's pizza
-    //       src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-    //       dest: 'dist/images/'                  // Destination path prefix
+    //       expand: true,
+    //       cwd: 'views/images/',            //Removed 'images_src/'
+    //       src: ['**/*.{gif,jpg,png}'],
+    //       dest: 'dist/images/'
     //     }]
     //   }
     // }
+    imagemin: {
+      dev: {
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'views/images/',           //Removed 'images_src' to minify images in Cam's pizza
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'dist/images/'                  // Destination path prefix
+        }]
+      }
+    },
+    uglify: {
+      build: {
+          src: 'js/perfmatters.js',
+          dest: 'dist/js/perfmatters.min.js'
+      }
+    }
   });
 
-  grunt.loadNpmTasks('grunt-responsive-images');
-  // grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.registerTask('default', 'responsive_images');
+  // grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['imagemin', 'uglify']);     //Removed 'responsive_images',
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
 };
